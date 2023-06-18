f5c2e95fa7d82c23b0e817261cd894da554dc641
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-06-18 14:43:21 +0530

    submitting code-Sun Jun 18 2023 14:43:19 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,106 +1,67 @@
-import React, { Component, useState } from 'react';
+import React, {useState} from "react";
 import '../styles/App.css';
+// import { set } from "cypress/types/lodash";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name1: '',
-      name2: '',
-      relationshipStatus: '',
-    };
-  }
 
-  handleName1Change = (event) => {
-    this.setState({ name1: event.target.value });
-  };
 
-  handleName2Change = (event) => {
-    this.setState({ name2: event.target.value });
-  };
+let arr = ["Siblings","Friends", "Love", "Affection", "Marriage", "Enemy"];
 
-  handleCalculateRelationship = () => {
-    const { name1, name2 } = this.state;
-    const name1Set = new Set(name1);
-    const name2Set = new Set(name2);
+const App = () => {
 
-    const remainingName1 = Array.from(name1).filter((char) => !name2Set.has(char)).join('');
-    const remainingName2 = Array.from(name2).filter((char) => !name1Set.has(char)).join('');
+    let [firstName, setFirstName] = useState("");  // Soumya
+    let [secondName, setSecondName] = useState(""); // ansh
+    let [relationShip, setRelationShip] = useState(""); // Love
+    let [click, setClick] = useState(false); 
 
-    const score = (remainingName1.length + remainingName2.length) % 6;
+    function findRelationShip(){
+        if(firstName === "" || secondName === ""){
+              return setRelationShip("Please Enter valid input")
+        }
 
-    let status;
-    switch (score) {
-      case 1:
-        status = 'Friends';
-        break;
-      case 2:
-        status = 'Love';
-        break;
-      case 3:
-        status = 'Affection';
-        break;
-      case 4:
-        status = 'Marriage';
-        break;
-      case 5:
-        status = 'Enemy';
-        break;
-      case 0:
-        status = 'Siblings';
-        break;
-      default:
-        status = '';
+
+         let string1,string2
+         for(let t of firstName){
+             if(secondName.includes(t)){
+                  setFirstName(firstName.replace(t,""))
+                //   string2 = secondName.replace(t,"")
+                  setSecondName(secondName.replace(t,""))
+             }
+         }
+         setClick(true)
+
+            // console.log(string1,string2)
+            // let count = string1.length + string2.length
+            // setRelationShip(arr[count%6])
     }
 
-    this.setState({ relationshipStatus: status });
-  };
 
-  handleClear = () => {
-    this.setState({
-      name1: '',
-      name2: '',
-      relationshipStatus: '',
-    });
-  };
 
-  render() {
-    const { name1, name2, relationshipStatus } = this.state;
+    return(
+        <div>
 
-    return (
-      <div id="main">
-        <h2>FLAMES Game</h2>
+                
+                <input type="text"  placeholder="First Name" data-testid="input1"
+                    onChange = {(event) => setFirstName(event.target.value)}
+                /> 
+                <input type="text"  placeholder="Second Name" data-testid="input2"
+                     onChange = {(event) => setSecondName(event.target.value)}
+                />
+                <button onClick={findRelationShip} data-testid="calculate_relationship">Calculate Relationship Future</button>
+                <button data-testid="clear" onClick={()=>{
+                    setFirstName("")
+                    setSecondName("")
+                    setRelationShip("")
+                    setClick(false)
+                }}>Clear</button>
+                <h3 data-testid="answer">{
+                    click &&
+                     arr[(firstName.length+secondName.length)%6]
+                }</h3>
 
-        <label htmlFor="name1">Enter First Name:</label>
-        <input
-          type="text"
-          id="name1"
-          value={name1}
-          onChange={this.handleName1Change}
-          data-testid="input1"
-        />
-
-        <label htmlFor="name2">Enter Second Name:</label>
-        <input
-          type="text"
-          id="name2"
-          value={name2}
-          onChange={this.handleName2Change}
-          data-testid="input2"
-        />
-
-        <button onClick={this.handleCalculateRelationship} data-testid="calculate_relationship">
-          Calculate Relationship Future
-        </button>
-
-        <h3 data-testid="answer">{relationshipStatus}</h3>
-
-        <button onClick={this.handleClear} data-testid="clear">
-          Clear
-        </button>
-      </div>
-    );
-  }
+                
+        </div>
+    )
 }
+
 
 export default App;
